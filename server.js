4fd5ea30848f245e50608f40f319e73e335c02aa
Rehan-013 <153925483+Rehan-013@users.noreply.github.com>
2025-10-23 18:12:32 +0530

    implement ai and basic socket functinality

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 require("dotenv").config();
 const app = require("./src/app");
 
+const { generateResponse } = require("./src/services/ai.service");
+
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, {/* options */});
+const io = new Server(httpServer, {
+  /* options */
+});
 
 io.on("connection", (socket) => {
-    // console.log("a user connected");
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("ai-message", async (data) => {
+    const response = await generateResponse(data.prompt);
+
+    console.log(response);
+
+    socket.emit("ai-message-response", { response });
+  });
 });
 
 const port = process.env.PORT || 3000;
 
 httpServer.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
